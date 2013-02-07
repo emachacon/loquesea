9d1d1112561c94cdcd18caaa6a199f60b13cd521
eydi <eydiangelica123@hotmail.com>
2013-02-07 17:01:35 -0500

    primeros cambios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loquesea::Application.config.secret_token = '508e650cfa88fe3a571cdda4f88ec256d86502e696515b987cd601e1787a69cf1c1b436fe77332e8cfd0f9d0f68c4187bf6329f3c9ae3229a3d9d08d80181632'
